d9c10056ae4e99c3c0ba6492ce09621d5b8d7073
Yannick <yannmath2708@gmail.com>
2026-08-26 20:15:57 +0200

    Added string length function

--- test.asm
+++ test.asm
@@ -1,5 +1,5 @@
 section .data
-    greeting1 db 'Hello', 0x0a
+    greeting1 db 'Hello', 0x0a, 0
     greeting1Len equ $ - greeting1
     greeting2 db 'World!', 0x0a
     greeting2Len equ $ - greeting2
@@ -9,24 +9,48 @@ section .text
     global _start
 
 _start:
-    mov rsi, greeting1
-    mov rdx, greeting1Len
-    call print
+    mov rdi, greeting1
+    call strlen
 
-    mov rsi, greeting2
-    mov rdx, greeting2Len
-    call print
+    ;mov rdi, greeting1
+    ;mov rsi, greeting1Len
+    ;call print
 
-    mov rsi, newline
-    mov rdx, 1 
-    call print
+    ;mov rdi, greeting2
+    ;mov rsi, greeting2Len
+    ;call print
 
+    ;mov rdi, newline
+    ;mov rsi, 1 
+    ;call print
+
+    mov rdi, rax
     mov rax, 60
-    mov rdi, 0
     syscall
 
 print:
+    ;RDI = address of string
+    ;RSI = length of string
+    mov rdx, rsi
+    mov rsi, rdi
     mov rax, 1
     mov rdi, 1
     syscall
+    ret
+
+strlen:
+    ;RDI = address of null-terminated string
+    ;RAX = resulting length
+    mov rax, 0
+
+.loop:
+    mov bl, [rdi]
+    cmp bl, 0
+    je .done
+
+    inc rax
+    inc rdi
+    jmp .loop
+
+.done:
     ret
